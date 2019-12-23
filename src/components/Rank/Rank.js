6be7a900f40eb33acf4f70edcb22cabff5bb11ed
Richard Okoye <emmanuel.eze@netopng.com>
2019-12-23 16:14:04 +0100

    add components

---
+++ src/components/Rank/Rank.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+
+const Rank = () => {
+    return (
+        <React.Fragment>
+        <div className="white f3">
+            {'Richard, your current rank is'}
+            </div>
+            <div className="white f3">
+                {'#5'}
+            </div>
+        </React.Fragment>
+    ) 
+}
+
+export default Rank;
